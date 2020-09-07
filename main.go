2956606c005b2d9b551374dc41e2bd40961471c4
François Paupier <francois.paupier@gmail.com>
2020-09-07 12:06:14 +0200

    add: perform read from the postgreSQL db

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres"
 	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/golang/protobuf/proto"
 	"log"
 )
 
@@ -37,18 +41,51 @@ func main() {
 		ev := consumer.Poll(0)
 		switch e := ev.(type) {
 		case *kafka.Message:
-			log.Printf("%% Message on %s:\n%s\n",
-				e.TopicPartition, string(e.Value))
+			alert := createAlert(e.Value)
+			persistAlert(alert)
 		case kafka.PartitionEOF:
 			log.Printf("%% Reached %v\n", e)
 		case kafka.Error:
 			log.Fatalf("%% Error: %v\n", e)
-			//default:
-			//	log.Printf("Ignored %v\n", e)
 		}
 	}
-
 	// Persist to Postgres
 
 	// Publish message to Kafka (notification queue)
+}
+
+// createAlert creates an alert from bytes.
+func createAlert(msg []byte) *Alert {
+	alert := &Alert{}
+	if err := proto.Unmarshal(msg, alert); err != nil {
+		log.Fatalf("failed to unmarshal kafka message into alert: %v\n", err)
+	}
+	return alert
+}
+
+// persistAlert saves the alert to a PostgreSQL store
+func persistAlert(alert *Alert) {
+	dsn := fmt.Sprintf("host=%s dbname=%s user=%s password=%s sslmode=disable",
+		INSTANCE_CONNECTION_NAME,
+		DATABASE_NAME,
+		DATABASE_USER,
+		PASSWORD)
+	db, err := sql.Open("cloudsqlpostgres", dsn)
+	if err != nil {
+		log.Fatalf("failed to open DB: %v\n", err)
+	}
+	rows, err := db.Query("SELECT id, model_name FROM face_detection_model")
+	if err != nil {
+		log.Fatalf("failed to query DB: %v\n", err)
+
+	}
+	var id string
+	var name string
+	for rows.Next() {
+		if err = rows.Scan(&id, &name); err != nil {
+			log.Fatalf("failed to scan row: %v\n", err)
+		}
+		fmt.Printf("Face detection model ID# %s name: `%s`\n", id, name)
+
+	}
 }
