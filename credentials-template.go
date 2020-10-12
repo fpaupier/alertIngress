ed0d1279d91f4fcf188e412a4c4e5b3374c5b7b3
François Paupier <francois.paupier@gmail.com>
2020-10-12 14:00:28 +0200

    add: sample credentials

---
+++ credentials-template.go
@@ -0,0 +1,11 @@
+package main
+
+const (
+	ConfluentServer        = "your confluent server"
+	ConfluentApiKey        = "your confluent api key"
+	ConfluentSecret        = "your confluent secret"
+	InstanceConnectionName = "your instance connection name"
+	DatabaseName           = "your db name"
+	DatabaseUser           = "your db user"
+	Password               = "your db password for the given user"
+)
